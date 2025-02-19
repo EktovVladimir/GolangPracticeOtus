5b5b4ad46fd02f8793d4db5c0603063c196f5844
Vladimir Ektov <ektov@mego.travel>
2025-02-19 23:47:43 +0300

    + hw7 board

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"github.com/EktovVladimir/GolangPracticeOtus/hw7"
 )
 
 func main() {
-	fmt.Println("Hello, Vladimir!")
+	var x, y int
+
+	fmt.Print("Ширина = ")
+	_, _ = fmt.Scanln(&x)
+	fmt.Print("Высота = ")
+	_, _ = fmt.Scanln(&y)
+
+	board := hw7.DrawBoard(x, y)
+
+	fmt.Println(board)
 }
